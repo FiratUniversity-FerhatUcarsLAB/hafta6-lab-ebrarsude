ba0c1cc32bac28896da57938802098c49f03a508
ebrarsude <250541104@firat.edu.tr>
2025-11-23 22:17:53 +0300

    Update Proje3_RestoranSiparis.java

--- Proje3_RestoranSiparis.java
+++ Proje3_RestoranSiparis.java
@@ -1,5 +1,7 @@
 Ad soyad:Ebrar Sude Yıldırım
 Öğrenci no:250541104
+Proje: Restoran sipariş
+Tarih:23.11.2025
 
   import java.util.Scanner;
 
@@ -175,3 +177,4 @@ public class SiparisSistemi {
         input.close();
     }
 }
+
